400420382043e18a21d2fc170d9a6ba8b85e9915
mspiritas <77894776+mspiritas@users.noreply.github.com>
2021-03-04 00:16:10 -0600

    Connected to JawsDB

--- model/database.php
+++ model/database.php
@@ -1,13 +1,13 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=todolist';
-    $username = 'root';
-    $password = 'sesame';
+    $dsn = 'mysql:host=qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=eo9ucrauoxsy638f';
+    $username = 'nbvkk02kqtfkhzro';
+    $password = 'wtw69ilw9pfius2d';
 
     try {
         $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error = "Database Error: ";
         $error .= $e->getMessage();
-        include('../view/error.php');
+        include('view/error.php');
         exit();
     }
